43de5b2cf185e0bb9d6fc6e1f84ca88f4d687e7b
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2018-10-28 20:51:35 -0500

    Local Copy

--- NodeJS/Assignment1/readwritefile.js
+++ NodeJS/Assignment1/readwritefile.js
@@ -4,13 +4,8 @@ var buffer = '';
 
 fs.readFile('temp.txt', function(err, buf) {
   console.log(buf.toString());
-  buffer = buf;
-});
-
-console.log(buffer);
-
-fs.writeFile('newfile.txt', buffer, function(err) {
+  fs.writeFile('newfile.txt', buf, function(err, buf) {
     if (err) throw err;
-
     console.log("File Write Completed");
+  });
 });
