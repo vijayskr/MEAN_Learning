fcd9045194f602348f7d3e8df9063bab389e10f8
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2019-01-14 19:57:25 -0600

    Server Side Updated Code
    Server Side Updated Code

--- Final_Project/backend/server.js
+++ Final_Project/backend/server.js
@@ -1,12 +1,18 @@
 var mongoose = require('mongoose');
 var cors  = require('cors');
 var bodyparser = require('body-parser');
-var mongoose = require('mongoose');
 var express = require('express');
 
-//File Upload & Download
-var gridfs = require("gridfs-stream");
-var fs = require('fs');
+let multer = require("multer");
+/*let gfs = require("multer-gridfs-storage")({
+  url: "mongodb://localhost:27017/test"
+});
+*/
+let grid = require("gridfs-stream");
+grid.mongo = mongoose.mongo;
+
+
+//var fs = require('fs');
 
 var user = require('./models/users');
 
@@ -18,7 +24,8 @@ app.use(bodyparser.json());
 mongoose.connect('mongodb://localhost:27017/test');
 const connection = mongoose.connection;
 mongoose.Promise = global.Promise;
-gridfs.mongo = mongoose.mongo;
+//let gfs = grid(mongoose.connection.db);
+
 
 connection.once('open', () => {
     console.log('Connected to MongoDB in port 27017');
@@ -57,52 +64,93 @@ router.route('/users/delete/:id').get((req, res) => {
     });
 });
 
-//Upload File to MongoDB
-router.route('/users/file/upload/:id').get((req, res) => {
-		
-		var filename = req.query.filename;
-        console.log('Inside Upload1');
-        
-        var writestream = gfs.createWriteStream({ filename: filename });
-        fs.createReadStream(__dirname + "/uploads/" + filename).pipe(writestream);
-        writestream.on('close', (file) => {
-            res.json('Stored File: ' + file.filename);
-        });
-        res.json('Inside the function call');
-        console.log('Inside Upload');
+/* FILE UPLOAD & DOWNLOAD */
+// Setting up the storage element
+let storage = require('multer-gridfs-storage')({
+   url: 'mongodb://localhost:27017/test',
+
+    filename: (req, file, cb) => {
+        let date = Date.now();
+        // The way you want to store your file in database
+        cb(null, file.fieldname + '-' + date + '.'); 
+    },
+    
+    // Additional Meta-data that you want to store
+    metadata: function(req, file, cb) {
+        cb(null, { originalname: file.originalname });
+    },
+    root: 'test' // Root collection name
 });
 
-//Download File from MongoDB
-router.route("/users/file/download/:id", (req, res) => {
-  var filename = req.query.filename;
+// Multer configuration for single file uploads
+let upload = multer({
+    storage: storage
+}).single('file');
 
-  gfs.exist({ filename: filename }, (err, file) => {
-    if (err || !file) {
-      res.status(404).send("File Not Found");
+// Route for file upload
+app.post("/users/file/upload/:id", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      res.json({ error_code: 1, err_desc: err });
       return;
     }
-
-    var readstream = gfs.createReadStream({ filename: filename });
-    readstream.pipe(res);
+    res.json({ error_code: 0, error_desc: null, file_uploaded: true });
   });
-});	
+});
 
 //Delete File from MongoDB
- router.route("/users/file/delete/:id", (req, res) => {
-   var filename = req.query.filename;
+app.get("/users/file/download/:id", (req, res) => {
+  gfs.collection("ctFiles"); //set collection name to lookup into
 
-   gfs.exist({ filename: filename }, (err, file) => {
-     if (err || !file) {
-       res.status(404).send("File Not Found");
-       return;
-     }
+  /** First check if file exists */
+  gfs.files
+    .find({ filename: req.params.filename })
+    .toArray(function(err, files) {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          responseCode: 1,
+          responseMessage: "error"
+        });
+      }
+      // create read stream
+      var readstream = gfs.createReadStream({
+        filename: files[0].filename,
+        root: "ctFiles"
+      });
+      // set the proper content type
+      res.set("Content-Type", files[0].contentType);
+      // Return response
+      return readstream.pipe(res);
+    });
+});
 
-     gfs.remove({ filename: filename }, err => {
-       if (err) res.status(500).send(err);
-       res.send("File Deleted");
-     });
-   });
- });
+/*
+// Route for getting all the files
+app.get('/files', (req, res) => {
+    let filesData = [];
+    let count = 0;
+    gfs.collection('ctFiles'); // set the collection to look up into
+
+    gfs.files.find({}).toArray((err, files) => {
+        // Error checking
+        if(!files || files.length === 0){
+            return res.status(404).json({
+                responseCode: 1,
+                responseMessage: "error"
+            });
+        }
+        // Loop through all the files and fetch the necessary information
+        files.forEach((file) => {
+            filesData[count++] = {
+                originalname: file.metadata.originalname,
+                filename: file.filename,
+                contentType: file.contentType
+            }
+        });
+        res.json(filesData);
+    });
+});
+*/
 
 app.use('/', router);
 app.listen(4000, () => { 
